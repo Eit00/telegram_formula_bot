089d8ff236dd48b798c2e02b6d8da36db9485735
Daulet Sergeev <95867411+Eit00@users.noreply.github.com>
2025-08-26 12:59:05 +0500

    up

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-# Конфиг проекта
 import os
+from dotenv import load_dotenv
 
+# Загружаем переменные из .env
+load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "PASTE_YOUR_BOTFATHER_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./raffle.db")
-
-
-# Доступ к простой админке (примитивная защита через секрет; можно заменить на auth)
 ADMIN_SECRET = os.getenv("ADMIN_SECRET", "change-me")
